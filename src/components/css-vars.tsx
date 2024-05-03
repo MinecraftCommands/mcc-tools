f419fa3bf206af9593091941598f14099d6a4b2e
Stefan VDV <vdvman88@gmail.com>
2024-05-03 21:21:28 +1000

    docs: 💡 Document the viewport height logic

--- src/components/css-vars.tsx
+++ src/components/css-vars.tsx
@@ -9,6 +9,13 @@ export default function CssVars({ children }: { children: React.ReactNode }) {
   return <div style={{ "--viewport-height": height }}>{children}</div>;
 }
 
+/**
+ * Custom hook that returns the current height of the viewport.
+ *
+ * Necessary because 100vh doesn't take into account scroll bars.
+ *
+ * @return {string} The height of the viewport in pixels or "100vh" on the server.
+ */
 function useViewportHeight() {
   const [height, setHeight] = useState("100vh");
   const updateHeight = useCallback(
@@ -16,11 +23,15 @@ function useViewportHeight() {
     [],
   );
 
+  // Track resizes including changes to whether the scroll bars exist or not
+  // Misses manual vertical window resizes
   useResizeObserver(
     typeof window !== "undefined" ? document.documentElement : null,
     updateHeight,
   );
 
+  // Track manual resizes of the window
+  // Misses changes to whether the scroll bars exist or not
   useEffect(() => {
     window.addEventListener("resize", updateHeight);
     updateHeight();
