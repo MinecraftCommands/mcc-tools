f4b323a0cf048f4121121932eabd847aa2ceaaa8
T0RNATO <nlittle315@gmail.com>
2024-05-06 20:03:44 +1000

    feat: Add styling for the hovered and currently selected version

--- src/components/java/version-link.tsx
+++ src/components/java/version-link.tsx
@@ -1,9 +1,11 @@
 import {
   HoverCard,
   HoverCardContent,
+  HoverCardPortal,
   HoverCardTrigger,
 } from "~/components/ui/hover-card";
 import Link from "next/link";
+import {clsx} from "clsx";
 
 export default function VersionLink({
   versionName,
@@ -19,14 +21,18 @@ export default function VersionLink({
   const url = `/java/changelog/${versionLink}`;
 
   return (
-    <HoverCard>
+    <HoverCard closeDelay={0}>
       <HoverCardTrigger asChild>
-        <Link href={url}>{versionName}</Link>
+        {/*TODO: Unhardcode this to show the currently selected page.*/}
+        <Link href={url} className={clsx('w-full inline-block hover:bg-white/20 p-1 px-3',
+          {'bg-white/30 underline': versionName == '1.20.6'})}>{versionName}</Link>
       </HoverCardTrigger>
-      <HoverCardContent className="prose prose-sm dark:prose-invert">
-        <h2>{title}</h2>
-        <p>{shortText}...</p>
-      </HoverCardContent>
+      <HoverCardPortal>
+        <HoverCardContent className="prose prose-sm dark:prose-invert w-80">
+          <h2>{title}</h2>
+          <p>{shortText}...</p>
+        </HoverCardContent>
+      </HoverCardPortal>
     </HoverCard>
   );
 }
