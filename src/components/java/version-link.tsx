86a11f89393f694521dc8c83a4739b2100fa1aea
T0RNATO <nlittle315@gmail.com>
2024-05-08 19:19:00 +1000

    fix: Made version hover styling work in light mode

--- src/components/java/version-link.tsx
+++ src/components/java/version-link.tsx
@@ -24,8 +24,8 @@ export default function VersionLink({
     <HoverCard closeDelay={0}>
       <HoverCardTrigger asChild>
         {/*TODO: Unhardcode this to show the currently selected page.*/}
-        <Link href={url} className={cn('w-full inline-block hover:bg-white/20 p-1 px-3',
-          {'bg-white/30 underline': versionName == '1.20.6'})}>{versionName}</Link>
+        <Link href={url} className={cn('w-full inline-block hover:bg-foreground/10 p-1 px-3',
+          {'bg-foreground/15 underline': versionName == '1.20.6'})}>{versionName}</Link>
       </HoverCardTrigger>
       <HoverCardPortal>
         <HoverCardContent className="prose prose-sm dark:prose-invert w-80">
