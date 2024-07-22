795917b1eb9615f776afc3a3c1c60f77ea238fab
Stefan VDV <vdvman88@gmail.com>
2024-07-22 17:22:41 +1000

    fix: 🐛 Force latest java changelog page to render dynamically
    This is a temporary hackfix, the ISR of this page needs to be investigated
    
    fixes #4

--- src/app/java/changelog/page.tsx
+++ src/app/java/changelog/page.tsx
@@ -1,5 +1,11 @@
 import PatchNotes from "~/components/java/patch-notes";
 
+// HACK: Temporary hackfix to ensure the latest patch notes page always shows the latest
+// For whatever reason this page does trigger ISR, but doesn't appear to update properly
+// Or at least it appears so, maybe it just takes longer than expected to get the new page
+// This needs to be investigated further, but it is difficult to do
+export const dynamic = "force-dynamic";
+
 export default async function LatestPatchNotes() {
   return <PatchNotes />;
 }
