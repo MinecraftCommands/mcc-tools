006b45074725f203ce60d59a4284e9953769969e
Stefan VDV <vdvman88@gmail.com>
2024-08-06 19:37:39 +1000

    fix: 💄 Increase font size on mobile for java patch notes

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -79,6 +79,15 @@ export default {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
       },
+      typography: {
+        sm: {
+          css: [
+            {
+              fontSize: `0.9375rem`,
+            },
+          ],
+        },
+      },
     },
   },
   plugins: [require("tailwindcss-animate"), require("@tailwindcss/typography")],
