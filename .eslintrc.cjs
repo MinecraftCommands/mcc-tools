4a142964446493c69ac459d1746c3b71eebc227b
Stefan VDV <vdvman88@gmail.com>
2024-10-25 10:01:50 +1100

    feat(internal): ✨ Add shadcn/ui sidebar, input, separator, tooltip, and useIsMobile

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -13,6 +13,7 @@ const config = {
     "plugin:@typescript-eslint/stylistic-type-checked"
   ],
   "rules": {
+    "@typescript-eslint/no-empty-interface": "off",
     "@typescript-eslint/array-type": "off",
     "@typescript-eslint/consistent-type-definitions": "off",
     "@typescript-eslint/consistent-type-imports": [
