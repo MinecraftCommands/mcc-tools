4f0654356113919ff1e5320dbcec5e1ed59ddd90
Stefan VDV <vdvman88@gmail.com>
2024-11-06 08:11:05 +1100

    fix: 🚸 Always use en-us locale for datetime format in changelog articles to match the language of the article

--- src/components/java/publish-date.tsx
+++ src/components/java/publish-date.tsx
@@ -1,22 +1,18 @@
 "use client";
 
 export function PublishDate({ date }: { date: Date }) {
-  const dateFormat: Intl.DateTimeFormatOptions = {
+  const publicationDate = new Date(date).toLocaleDateString("en-us", {
     day: "numeric",
     weekday: "long",
     month: "long",
     year: "numeric",
     hour: "numeric",
     minute: "numeric",
-  };
-  const publicationDate = new Date(date).toLocaleDateString(
-    undefined, // Uses the client's timezone
-    dateFormat,
-  );
+  });
 
   return (
     <span
-      className="dark:text-shadow-around text-sm font-semibold text-gray-700 dark:text-gray-400 dark:shadow-gray-950"
+      className="text-sm font-semibold text-gray-700 dark:text-gray-400 dark:shadow-gray-950 dark:text-shadow-around"
       suppressHydrationWarning // Prevents React hydration warning caused by time localisation
     >
       {publicationDate}
