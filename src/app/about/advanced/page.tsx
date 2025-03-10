b36b1948411694be5939c93fbb6b335c38459f40
Stefan VDV <vdvman88@gmail.com>
2025-03-11 09:35:03 +1100

    feat(internal): 🚧 Try to print out the branch name of a PR, both in GH:A and from Vercel
    This is a test to check what the values actually are for use when making per-pr DB branches

---
+++ src/app/about/advanced/page.tsx
@@ -0,0 +1,57 @@
+import { env } from "~/env";
+
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "~/components/ui/table";
+
+export const dynamic = "force-dynamic";
+
+export default function AdvancedSiteInfo() {
+  // WARNING: Be **very** careful about what you expose here,
+  // since this is a server component it is able to expose server side only environment variables,
+  // which includes things like API tokens and other secrets
+  // **DO NOT** expose things that must be secret
+
+  return (
+    <main className="container prose pt-8 lg:prose-xl dark:prose-invert">
+      <h1>Advanced information about MCC Gadgets</h1>
+      <p>
+        Warning! Here be dragons! This page presents various pieces of advanced
+        information about the website, you probably don&apos;t need to know
+        about this stuff, maybe think again about what brought you to this page
+      </p>
+      <p>
+        Eventually this page will be locked behind specific permissions, but
+        currently that isn&apos;t implemented
+      </p>
+      <Table>
+        <TableHeader>
+          <TableRow>
+            <TableHead>Key</TableHead>
+            <TableHead>Value</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          <EnvVarRow envVar="VERCEL_GIT_COMMIT_REF" />
+        </TableBody>
+      </Table>
+    </main>
+  );
+}
+
+function EnvVarRow({ envVar }: { envVar: keyof typeof env }) {
+  const value = env[envVar];
+  return (
+    <TableRow>
+      <TableCell>{envVar}</TableCell>
+      <TableCell>
+        {typeof value === "undefined" ? "undefined" : value}
+      </TableCell>
+    </TableRow>
+  );
+}
