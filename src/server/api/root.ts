9858e36a4d9c51eb9189d53fad6d55fe32f2510f
Stefan VDV <vdvman88@gmail.com>
2025-04-30 15:09:55 +1000

    fix(internal): 🔥 Properly remove all references to the template "post" data

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,4 +1,4 @@
-import { postRouter } from "~/server/api/routers/post";
+//import { postRouter } from "~/server/api/routers/post";
 import { createCallerFactory, createTRPCRouter } from "~/server/api/trpc";
 
 /**
@@ -7,7 +7,7 @@ import { createCallerFactory, createTRPCRouter } from "~/server/api/trpc";
  * All routers added in /api/routers should be manually added here.
  */
 export const appRouter = createTRPCRouter({
-  post: postRouter,
+  //post: postRouter,
 });
 
 // export type definition of API
